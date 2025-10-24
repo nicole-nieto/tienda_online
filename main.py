78788e8dc54b856beb2ef90f519f851cd8edfacf
Nicole <nicolnieto48@gmail.com>
2025-10-24 17:34:51 -0500

    correccion de importacion de ruta de categoria

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from core.db import init_db
-from routers import category_router, product_router
+from routers.category_router import router as category_router
+from routers.product_router import router as product_router
 
 app = FastAPI(title="Sistema de Gestión de Tienda Online")
 
-app.include_router(category_router.router)
-app.include_router(product_router.router)
+# Incluir los routers
+app.include_router(category_router)
+app.include_router(product_router)
 
+# Evento que inicializa la base de datos al iniciar la app
 @app.on_event("startup")
 def on_startup():
     init_db()
